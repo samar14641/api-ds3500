2ada2c8ec102fc00e8f9e3ef752c4c15d83bef0e
Samar Dikshit <samard05@gmail.com>
2021-10-20 13:36:40 -0400

    added v2 orders api, and api key auth for v2

--- full_api.py
+++ full_api.py
@@ -6,6 +6,7 @@ from flask import abort, Flask, jsonify, request
 from typing import Final
 
 
+# PriorityValueError: custom exception class
 # DatabaseSimulator: class
 # abort: function
 # Flask: class
@@ -46,6 +47,22 @@ def bad_request(error):
         'message': error.description
     })
     res.status_code = 400
+
+    return res
+
+@app.errorhandler(401)
+def unauthorized(error):
+    """Custom 401 error handler
+    Parameters:
+        error (dict): the error
+    Returns:
+        flask.Response"""
+
+    res =  jsonify({
+        'error': True,
+        'message': error.description
+    })
+    res.status_code = 401
 
     return res
 
@@ -142,10 +159,51 @@ def get_orders_by_priority():
                 'meta': {'message': 'full pagination not implemented'}
             })
         else:
-            return no_content('No orders with priority {}'.format(ord_priority))
+            return no_content()
 
     else:
         return abort(400, 'Missing priority value')
+
+def verify_key(api_key) -> bool:
+    """Verify the API key for v2 APIs
+    Parameters:
+        api_key (str): the API key
+    Returns:
+        bool: whether the key is valid or not"""
+
+    try:
+        if len(api_key) == 5 and int(api_key) % 3 == 0:  # a valid key has 5 digits AND is divisible by 3 
+            return True
+        else:
+            return False
+    
+    except ValueError:
+        return False
+
+@app.route('/ds3500/api/v2/orders', methods = ['GET'])
+def get_orders_auth():
+    """Get all orders upto page limit after auth
+    (Note: full pagination not implemented)
+    Returns:
+        flask.Response: API response"""
+
+    api_key = request.headers.get('x-api-key', None)
+    
+    if api_key is not None and verify_key(api_key):  # key is present and valid
+        has_more = True if len(data) > PAGE_SIZE else False
+
+        return jsonify({
+            'size': len(data[: PAGE_SIZE]),
+            'data': data[: PAGE_SIZE],
+            'has_more': has_more,
+            'error': False,
+            'meta': {'message': 'full pagination not implemented'}
+        })
+
+    elif api_key is not None and not verify_key(api_key):  # key is present and invalid i.e. HTTP 401
+        return abort(401, description = 'Invalid API key')
+    else:  # key not present
+        return abort(401, description = 'Missing API key')
 
     
 if __name__ == '__main__':
